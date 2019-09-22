a1f24dc1fb7229986f930f2acd86ffe182b638e0
FreakyCoder <kurayogun@gmail.com>
2019-09-22 21:32:18 +0300

    ImageContainer is refactored

--- example/lib/src/components/ImageContainer/ImageContainer.js
+++ example/lib/src/components/ImageContainer/ImageContainer.js
@@ -1,45 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {Text, View, Image} from 'react-native';
+import {Image} from 'react-native';
 import Androw from 'react-native-androw';
+import styles from './ImageContainer.style';
+
+const defaultImage =
+  'https://images.unsplash.com/photo-1538689621163-f5be0ad13ec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80';
 
 const ImageContainer = props => {
-  const {example} = props;
+  const {shadowStyle} = props;
   return (
-    <Androw
-      style={{
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowOpacity: 0.4,
-        shadowRadius: 6,
-      }}>
+    <Androw style={shadowStyle}>
       <Image
         borderRadius={24}
-        style={{
-          height: 170,
-          width: 115,
-          position: 'absolute',
-          top: -16,
-          left: -48,
-        }}
+        style={styles.imageStyle}
         source={{
-          uri:
-            'https://images.unsplash.com/photo-1538689621163-f5be0ad13ec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+          uri: defaultImage,
         }}
+        {...props}
       />
     </Androw>
   );
 };
 
-ImageContainer.propTypes = {
-  example: PropTypes.number,
-};
-
 ImageContainer.defaultProps = {
-  example: 5,
+  shadowStyle: styles.shadowStyle,
 };
 
 export default ImageContainer;
--- example/lib/src/components/ImageContainer/ImageContainer.style.js
+++ example/lib/src/components/ImageContainer/ImageContainer.style.js
@@ -1,1 +1,18 @@
-export default {};
+export default {
+  shadowStyle: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 6,
+  },
+  imageStyle: {
+    top: -16,
+    left: -48,
+    width: 115,
+    height: 170,
+    position: 'absolute',
+  },
+};
